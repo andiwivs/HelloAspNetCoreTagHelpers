2bf649c8f27b85cd6e89b40599343e474876d5e7
Andrew Withers <andiwivs@gmail.com>
2018-04-18 23:26:10 +0100

    Add mvc with empty home controller and view

---
+++ HelloAspNetCoreTagHelpers.Site/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloAspNetCoreTagHelpers.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
