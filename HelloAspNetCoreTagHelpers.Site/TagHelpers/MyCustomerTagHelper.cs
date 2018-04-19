23af3fd8bf159ea20e62ed536acc69fc4ce7fd6a
Andrew Withers <andiwivs@gmail.com>
2018-04-19 23:23:19 +0100

    Add basic tag helper example

---
+++ HelloAspNetCoreTagHelpers.Site/TagHelpers/MyCustomerTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace HelloAspNetCoreTagHelpers.Site.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("my-customer")]
+    public class MyCustomerTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetHtmlContent($"MyCustomerTagHelper says it is currently {DateTime.Now}");
+            output.TagName = "em";
+        }
+    }
+}
